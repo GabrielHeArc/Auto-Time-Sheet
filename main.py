bb11ca2452b8ae3241de999b1e6db3065f3fcf67
Gabriel Fasano <gabriel.fasano@wise.swiss>
2023-03-19 21:12:17 +0100

    Fix error in condition

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
     get_insert = requests.get(url)
     data = get_insert.json()[f"{get_month()}{get_year()}"][get_moment()]
 
-    if not (data == "{:d}:{:02d}".format(get_time().hour, get_time().minute)):
+    if (data == "{:d}:{:02d}".format(get_time().hour, get_time().minute)):
         send_notification(f"Hour inserted in {get_moment(True)}: " + "{:d}:{:02d}".format(get_time().hour, get_time().minute))
         print("Success")
     else:
