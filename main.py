1fe3416f2400df5874930dbc2fe500724d465ee6
Gabriel Fasano <gabriel.fasano@wise.swiss>
2023-03-23 13:54:02 +0100

    Add quota exceeded exception

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from http.client import PAYMENT_REQUIRED
 import os
 import requests
 from tools import *
@@ -13,21 +14,27 @@ def main():
         }
     })
 
-    if (resp.status_code == 200):
-        pass
 
-    else:
-        pass
-
-    get_insert = requests.get(url)
-    data = get_insert.json()[f"{get_month()}{get_year()}"][get_moment()]
-
-    if (data == "{:d}:{:02d}".format(get_time().hour, get_time().minute)):
-        send_notification(f"Hour inserted in {get_moment(True)}: " + "{:d}:{:02d}".format(get_time().hour, get_time().minute))
-        print("Success")
-    else:
-        send_notification(f"Error while insert hour in {get_moment(True)}")
-        print("Failure")
+    try:
+        if (resp.status_code == 402):
+            raise Exception("Quota exceeded")
+        get_insert = requests.get(url)
+        data = get_insert.json()[f"{get_month()}{get_year()}"][get_moment()]
+        retry = 0
+        success = False
+        while retry < 4 or not success:
+            if (data == "{:d}:{:02d}".format(get_time().hour, get_time().minute)):
+                send_notification(f"Hour inserted in {get_moment(True)}: " + "{:d}:{:02d}".format(get_time().hour, get_time().minute))
+                print("Success")
+                success = True
+            else:
+                send_notification(f"Error while insert hour in {get_moment(True)}")
+                retry += 1
+                print("Failure.. retrying")
+    except Exception as e:
+        print(e)
+        send_notification(f"Error while insert hour in {get_moment(True)}: " + str(e))
+        exit
 
 if __name__ == "__main__":
     main()
